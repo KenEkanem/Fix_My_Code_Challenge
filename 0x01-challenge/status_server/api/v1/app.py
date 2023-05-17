99653bbfc4198ffcb85a663cab6908076b82cc57
Kennedy Ekanem <kennedyekanem@gmail.com>
2023-05-17 05:43:40 +0100

    Updated code

--- 0x01-challenge/status_server/api/v1/app.py
+++ 0x01-challenge/status_server/api/v1/app.py
@@ -1,20 +1,22 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
-Web server 
+Web server Flask.
 """
-from api.v1.views import app_views
+
 from flask import Flask, jsonify, make_response
+from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-
 @app.errorhandler(404)
 def not_found(error):
-    """ json 404 page """
+    """Return a JSON-formatted 404 error response."""
     return make_response(jsonify({"error": "Not found"}), 404)
 
+def main():
+    """Run the web server."""
+    app.run(host="0.0.0.0", port=5000)
 
 if __name__ == "__main__":
-    # python -m api.v1.app 
-    app.run(host="0.0.0.0", port=5000)
+    main()
